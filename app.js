1b8a84a7ee164c9dad33b0532a1129f5edf16d61
yeongseok.yun <yeongseok.yun@saltlux.com>
2023-09-06 17:58:40 +0900

    [feat]마리아db연동 및 api 통신 테스트 완료

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
 
+const maria = require('./database/connect/maria')
+maria.connect();
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.send('Hello, Express')
+})
+
+app.get('/test',(req,res) =>{
+    maria.query('select * from pot.POT_USER', function(err, rows, fields){
+        if(!err){
+            console.log("succ");
+            res.send(rows)
+        }else{
+            console.log("err :" + err)
+        }
+    })
+})
+
+app.get('/join',(req,res) =>{
+    maria.query('select * from pot.POT_USER', function(err, rows, fields){
+        if(!err){
+            console.log("succ");
+            res.send(rows)
+        }else{
+            console.log("err :" + err)
+        }
+    })
 })
 
 app.listen(app.get('port'), () => {
